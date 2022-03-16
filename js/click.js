a950d66d39f3eca85c6af1e4dd1c6f1604d0858b
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-16 23:00:16 +0800

    x
    x

--- js/click.js
+++ js/click.js
@@ -1,4 +1,4 @@
-var Sfx = new Audio("https://cdn.jsdelivr.net/gh/AyGemuy/Cerdasin@master/typewriter.wav");
+var Sfx = new Audio("https://cdn.jsdelivr.net/gh/AyGemuy/Cerdasin/audio@master/typewriter.wav");
 
 document.onclick = function() {
   Sfx.play();
