87015fe1e04f7620a17760fb8a9c09a9c356fdb5
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-19 13:39:39 +0800

    x
    x

---
+++ js/safelinks.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+/* Pengaturan safeLink */
+var setTimer = 10; //waktu detik
+var setColor = '#f89000'; //warna loading timer
+var setText = 'Harap Tunggu...'; //pesan pada tombol
+var setCopyUrl = 'Salin URL'; // generator salin
+var setCopied = 'URL Tersalin'; //generator tersalin
+
+// global script version 1.1 open source code
+function safeLDefer(){var d=document.createElement("script");d.src="https://cdn.jsdelivr.net/gh/choipanwendy/safelink-v1.1@main/wcsafelink.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",safeLDefer,!1):window.attachEvent?window.attachEvent("onload",safeLDefer):window.onload=safeLDefer;
+//]]> 
