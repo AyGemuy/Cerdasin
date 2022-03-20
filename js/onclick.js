f8dba238853be8c351526ca34dedfba2dcb56e6f
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-20 11:42:11 +0800

    x
    x

---
+++ js/onclick.js
@@ -0,0 +1,33 @@
+var kliks = new Audio('https://www.soundjay.com/buttons/sounds/button-09a.mp3');
+function add-bookmark() {
+Toast.fire({
+ icon: 'success',
+ title: 'Sukses menambahkan ke bookmark'
+})
+kliks.currentTime = 0;
+kliks.play();
+}
+function huruf-kecil() {
+Toast.fire({
+ icon: 'success',
+ title: 'Sukses mengubah ke huruf kecil'
+})
+kliks.currentTime = 0;
+kliks.play();
+}
+function huruf-sedang() {
+Toast.fire({
+ icon: 'success',
+ title: 'Sukses mengubah ke huruf sedang'
+})
+kliks.currentTime = 0;
+kliks.play();
+}
+function huruf-besar() {
+Toast.fire({
+ icon: 'success',
+ title: 'Sukses mengubah ke huruf besar'
+})
+kliks.currentTime = 0;
+kliks.play();
+}
