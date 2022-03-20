f8ed9d269df3075b63cea6b2730c1eeb3289d11b
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-20 14:40:48 +0800

    x
    x

--- js/onclick.js
+++ js/onclick.js
@@ -3,6 +3,7 @@
 	var ngeklik2 = document.getElementById("huruf-kecil");
 	var ngeklik3 = document.getElementById("huruf-sedang");
 	var ngeklik4 = document.getElementById("huruf-besar");
+	var ngeklik5 = document.getElementById("di-follow");
 	
 	ngeklik.addEventListener("click", function () {
 Toast.fire({
@@ -27,4 +28,25 @@ Toast.fire({
  icon: 'success',
  title: 'Sukses mengubah ke huruf besar'
 })
+;} , false);
+	ngeklik5.addEventListener("click", function () {
+var { isConfirmed: di_follow } = swals.fire({
+            title: 'Follow yuk kak :3',
+            imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
+            imageWidth: 100,
+            imageHeight: 100,
+            imageAlt: 'Hai',
+            text: 'Mau follow tydack!',
+            showCancelButton: true,
+            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yups',
+            cancelButtonText: '<i class="fa fa-thumbs-down"></i> Tydack',
+            footer: 'By <a href="/"> Cerdasin62.</a>'
+            });
+            
+            if(di_follow){
+            await Toast.fire({
+  icon: 'success',
+  title: 'Okey!, Makasih ya kack'
+  });
+ } 
 ;} , false);
