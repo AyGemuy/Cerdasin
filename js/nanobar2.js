c4a2e70b49ef54d72d2b44b1e545b751307287ce
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-21 18:27:14 +0800

    x
    x

--- js/nanobar2.js
+++ js/nanobar2.js
@@ -6,15 +6,3 @@ var nanobar = new Nanobar( options );
 nanobar.go( 30 );
 nanobar.go( 76 );
 nanobar.go(100);
-
-var element = document.documentElement,
-  body = document.body,
-  scrollTop = 'scrollTop',
-  scrollHeight = 'scrollHeight',
-  progress = document.querySelector('.progress-bar'),
-  scroll;
-
-document.addEventListener('scroll', function() {
-  scroll = (element[scrollTop]||body[scrollTop]) / ((element[scrollHeight]||body[scrollHeight]) - element.clientHeight) * 100;
-  progress.style.setProperty('--scroll', scroll + '%');
-});
