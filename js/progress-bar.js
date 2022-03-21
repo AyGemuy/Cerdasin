ad81caf157c3d7c27b097e2344419d0bf9210e1d
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-21 17:11:13 +0800

    x
    x

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {
-    myFunction()
+    progress-atas()
 };
-function myFunction(){
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById(&quot;myBar&quot;).style.width = scrolled + &quot;%&quot;;
+function progress-atas(){
+    var DiGeser = document.body.scrollTop || document.documentElement.scrollTop;
+    var Tingginya = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var KuGeser = (DiGeser / Tingginya) * 100;
+    document.getElementById("myBar").style.width = KuGeser + "%";
 }
