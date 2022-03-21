3e36193cb8c8e43d77175b6d446a68735be1efc6
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-21 17:06:16 +0800

    x
    x

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -5,5 +5,5 @@ function myFunction(){
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
+    document.getElementById(&quot;myBar&quot;).style.width = scrolled + &quot;%&quot;;
 }
