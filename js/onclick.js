16911db82c1fd30f74c03b105259ebc83e650e35
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-23 21:59:50 +0800

    x
    x

--- js/onclick.js
+++ js/onclick.js
@@ -1,57 +1,5 @@
-$('#add-bookmark').on('click', function () {
-Toast.fire({
- icon: 'success',
- title: 'Sukses menambahkan ke bookmark'
-})
-});
 
-	$('#huruf-kecil').on('click', function () {
-Toast.fire({
- icon: 'success',
- title: 'Sukses mengubah ke huruf kecil'
-})
-});
-
-	$('#huruf-sedang').on('click', function () {
-Toast.fire({
- icon: 'success',
- title: 'Sukses mengubah ke huruf sedang'
-})
-});
-
-	$('#huruf-besar').on('click', function () {
-Toast.fire({
- icon: 'success',
- title: 'Sukses mengubah ke huruf besar'
-})
-});
-
-	$('#di-follow').on('click', function () {
-	swals.fire({
-            title: 'Follow yuk kak :3',
-            imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
-            imageWidth: 100,
-            imageHeight: 100,
-            imageAlt: 'Hai',
-            text: 'Mau follow tydack!',
-            showCancelButton: true,
-            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yups',
-            cancelButtonText: '<i class="fa fa-thumbs-down"></i> Tydack',
-            footer: 'By <a href="/"> Cerdasin62.</a>'
-            }).then((result) => {
-  if (result.isConfirmed) {
-     window.open("https://www.blogger.com/follow.g?blogID=8384880727344316369");
-  } else {
-            Toast.fire({
-  icon: 'warning',
-  title: 'Prank ya kack!'
-  })
-}
-})
-});
-            
-            $('#check-ip').on('click', function () {
-var { isConfirmed: check-ip } = await swals.fire({
+            var { isConfirmed: komfir-ip } = await swals.fire({
             title: 'Hai kak :3',
             imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
             imageWidth: 100,
@@ -63,17 +11,15 @@ var { isConfirmed: check-ip } = await swals.fire({
             footer: 'By <a href="/"> Cerdasin62.</a>'
             });
             
-            if(check-ip){
-            await Toast.fire({
-  icon: 'success',
-  title: 'Oke!, Selamat membaca ya'
-  onOpen: () => {
+            if(komfir-ip){
+            onOpen: () => {
     fetch('https://api.ipify.org?format=json')
       .then(response => response.json())
       .then(data => {
-        Swal.insertQueueStep(data.ip)
+        Swal.fire({
+  icon: 'success',
+  title: 'Your Ip '+ data.ip +' ok!'
+  });
       })
   }
-  });
-            }
-  });
+     }
