4229877e30d7190676a4dcbfdb55bbb8978e32a7
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-23 22:39:13 +0800

    x
    x

--- js/onclick.js
+++ js/onclick.js
@@ -28,50 +28,32 @@ Toast.fire({
 
 $('#di-follow').on('click', function () {
 	swals.fire({
-            title: 'Follow yuk kak :3',
-            imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
-            imageWidth: 100,
-            imageHeight: 100,
-            imageAlt: 'Hai',
-            text: 'Mau follow tydack!',
-            showCancelButton: true,
-            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yups',
-            cancelButtonText: '<i class="fa fa-thumbs-down"></i> Tydack',
-            footer: 'By <a href="/"> Cerdasin62.</a>'
-            }).then((result) => {
-  if (result.isConfirmed) {
-     window.open("https://www.blogger.com/follow.g?blogID=8384880727344316369");
-  } else {
-            Toast.fire({
-  icon: 'warning',
-  title: 'Prank ya kack!'
-  })
+ title: 'Follow yuk kak :3',
+ imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
+ imageWidth: 100,
+ imageHeight: 100,
+ imageAlt: 'Hai',
+ text: 'Mau follow tydack!',
+ showCancelButton: true,
+ confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yups',
+ cancelButtonText: '<i class="fa fa-thumbs-down"></i> Tydack',
+ footer: 'By <a href="/"> Cerdasin62.</a>'
+ }).then((result) => {
+ if (result.isConfirmed) {
+ window.open("https://www.blogger.com/follow.g?blogID=8384880727344316369");
+ } else {
+ Toast.fire({
+ icon: 'warning',
+ title: 'Prank ya kack!'
+ })
 }
 })
 });
-            
-            $('#check-ip').on('click', function () {
 
-            var { isConfirmed: komfir-ip } = swals.fire({
-            title: 'Hai kak :3',
-            imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
-            imageWidth: 100,
-            imageHeight: 100,
-            imageAlt: 'Hai',
-            text: 'Siap untuk Cek ip!',
-            showCancelButton: true,
-            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Check',
-            footer: 'By <a href="/"> Cerdasin62.</a>'
-            });
-            
-            if(komfir-ip){
-            
-        fetch('https://api.ipify.org?format=json')
-      .then(response => response.json())
-      .then(data => {
-        alert(data.ip)
-  })
-  
-  }
-  
-  });
+$('#check-ip').on('click', function () {
+ fetch('https://api.ipify.org?format=json')
+ .then(response => response.json())
+ .then(data => {
+ alert(data.ip)
+ })
+ });
