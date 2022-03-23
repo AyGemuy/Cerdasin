6d82aeb062418335df5d1231000346697673affc
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-23 22:31:25 +0800

    x
    x

--- js/onclick.js
+++ js/onclick.js
@@ -52,21 +52,24 @@ $('#di-follow').on('click', function () {
             
             $('#check-ip').on('click', function () {
 
-            swals.fire({
+            var { isConfirmed: komfir-ip } = await swals.fire({
             title: 'Hai kak :3',
             imageUrl: 'https://cdn.jsdelivr.net/gh/AyGemuy/Blogger/img/chat.png',
             imageWidth: 100,
             imageHeight: 100,
             imageAlt: 'Hai',
-            text: 'Ini ip Anda',
+            text: 'Siap untuk Cek ip!',
             showCancelButton: true,
-            cancelButtonText: '<i class="fa fa-thumbs-up"></i> Ok',
+            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Check',
             footer: 'By <a href="/"> Cerdasin62.</a>'
             });
             
-            fetch('https://api.ipify.org?format=json')
+            if(komfir-ip){
+            
+        fetch('https://api.ipify.org?format=json')
       .then(response => response.json())
       .then(data => {
-        alert(data.ip)
-  })
+       alert('Your IP :' + data.ip + '')
+  }
+  
   });
