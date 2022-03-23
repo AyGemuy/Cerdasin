75a6ebef561613270eef56b4a275de3f0a391ecf
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-23 21:23:21 +0800

    x
    x

--- js/onclick.js
+++ js/onclick.js
@@ -51,22 +51,22 @@ Toast.fire({
 })
             
             $('#check-ip').on('click', function () {
-    swal.queue([
-      {
-        title: 'Your public IP',
-        confirmButtonText: 'Show my public IP',
-        text: 'Your public IP will be received via AJAX request',
-        currentProgressStep: 0,
-        showLoaderOnConfirm: true,
-        preConfirm: function () {
-          return new Promise(function (resolve) {
-            $.get('https://api.ipify.org?format=json')
-              .done(function (data) {
-                swal.insertQueueStep(data.ip)
-                resolve()
-              })
-          })
-        }
-      }
-    ]).catch(swal.noop)
+swal.queue
+([{
+    title: 'Your public IP',
+    confirmButtonText: 'Show my public IP',
+    text: 'Your public IP will be received via AJAX request',
+    showLoaderOnConfirm: true,
+    preConfirm: function()
+    {
+        return new Promise(function (resolve)
+        {
+            $.get('https://api.ipify.org?format=json').done(function(data)
+            {
+                swal.insertQueueStep(data.ip);
+                resolve();
+            });
+        });
+    }
+}])
   })
