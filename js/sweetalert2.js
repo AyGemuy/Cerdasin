bf09085d2f9cc0f38318ab18f837fd1fc9f84132
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-24 17:40:33 +0800

    x
    x

--- js/sweetalert2.js
+++ js/sweetalert2.js
@@ -1,3 +1,4 @@
+/* SweetAlert */
 const swals = Swal.mixin({backdrop: 'rgba(0,0,0,0.4)', confirmButtonColor: '#003EFF', cancelButtonColor: '#FF0040', showCloseButton: true, allowOutsideClick: false,
 showClass: {
     popup: 'animate__animated animate__rotateInDownLeft'
@@ -7,6 +8,7 @@ showClass: {
   }
   });
 
+/* Toast */
 const Toast = Swal.mixin({
   toast: true,
   position: 'center',
@@ -19,6 +21,8 @@ const Toast = Swal.mixin({
   }
   });
 
+/* Sweet Alert Img */
+/*
             async function mulai(){
             var { isConfirmed: ke_satu } = await swals.fire({
             title: 'Hai kak :3',
@@ -42,6 +46,38 @@ const Toast = Swal.mixin({
             await swals.fire({
   icon: 'warning',
   title: 'Gak bisa kak, isi dulu!'
+  });
+            mulai();
+            }
+            }
+            mulai();
+*/
+
+/* Sweet Alert */
+async function mulai(){
+            var { isConfirmed: ke_satu } = await swals.fire({
+            title: 'Apakah halaman lambat?',
+            icon: 'question',
+            text: 'Silahkan jawab oke!',
+            showCancelButton: true,
+            confirmButtonText: '<i class="fa fa-thumbs-up"></i> Yups',
+            cancelButtonText: '<i class="fa fa-thumbs-down"></i> Tydack',
+            footer: 'By <a href="/"> Cerdasin62.</a>'
+            });
+            
+            if(ke_satu){
+            let katanya = ["Maklum masih pemula","Mohon maaf jika lemot","Oke","Ingfo di terima"]
+            let tadaa = Math.floor(Math.random() * katanya);
+            await swals.fire({
+  icon: 'info',
+  title: ' + tadaa + '
+  });
+            } else {
+            let katanye = ["Alhamdulilah ya","Makasih","Oke","Ingfo di terima"]
+            let tadee = Math.floor(Math.random() * katanye);
+            await swals.fire({
+  icon: 'info',
+  title: ' + tadee + '
   });
             mulai();
             }
