77944af935a8b5d84271d76d875539a26f951b66
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-26 23:14:00 +0800

    x
    x

---
+++ js/pre-copy.js
@@ -0,0 +1,3 @@
+/*<![CDATA[*/
+/* Pre Content Copy */ for(var preClick=document.getElementsByTagName("pre"),i=0;i<preClick.length;i++)preClick[i].addEventListener("dblclick",function(){var e=getSelection(),o=document.createRange();o.selectNodeContents(this),e.removeAllRanges(),e.addRange(o),document.execCommand("copy"),e.removeAllRanges(),document.querySelector("#tNf").innerHTML="<span><i class='clipboard'></i>Copied to clipboard!</span>"},!1);
+/*]]>*/
