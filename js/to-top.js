659405c0e7841fbc151bb3e4c4153f7dfb53bf70
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-27 16:58:49 +0800

    x
    x

--- js/to-top.js
+++ js/to-top.js
@@ -1,7 +1,12 @@
-$(document).scroll(function() {
-  return $(document).scrollTop() > 400 ? $('.BackToTop').addClass('show') : $('.BackToTop').removeClass('show')
-  }), $('.BackToTop').click(function() {
-    return $('html,body').animate({
-      scrollTop: '0'
-    });
+$(window).scroll(function() {
+    if($(this).scrollTop() &gt; 200) {
+        $(&#39;#back-to-top&#39;).fadeIn();
+    } else {
+        $(&#39;#back-to-top&#39;).fadeOut();
+    }
+});
+
+$(&#39;#back-to-top&#39;).hide().click(function() {
+    $(&#39;html, body&#39;).animate({scrollTop:0}, 1000);
+    return false;
 });
