4e2cef8630414170d3b1c084cdd0abcd938da488
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-27 15:10:18 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,8 +1,4 @@
-var sfont = document.getElementById("fontArea");
-	var fontBtn = document.getElementById("fontKecil");
-	var fontBtn2 = document.getElementById("fontSedang");
-	var fontBtn3 = document.getElementById("fontBesar");
-	
-	fontBtn.addEventListener("click", function () {sfont.style.fontSize ="xx-small";} , false);
-	fontBtn2.addEventListener("click", function () {sfont.style.fontSize ="medium";} , false);
-	fontBtn3.addEventListener("click", function () {sfont.style.fontSize ="xx-large";} , false);
+function FungsiHuruf(PilihTag) {
+  var listValue = PilihTag.options[PilihTag.selectedIndex].text;
+  document.getElementById("fontArea").style.fontSize = listValue;
+}
