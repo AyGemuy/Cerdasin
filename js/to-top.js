4904517e248916123646582e70c9a9d67caa3127
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 07:52:03 +0800

    x
    x

--- js/to-top.js
+++ js/to-top.js
@@ -1,34 +1,19 @@
-jQuery(window).scroll(function(){
-    if(jQuery(window).scrollTop()<50){
-        jQuery('#rocketmeluncur').slideUp(500);
-    }else{
-        jQuery('#rocketmeluncur').slideDown(500);
-    }
-    var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
-    var scrolltoprocketmeluncur = $('rocketmeluncur');
-var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
-var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
-var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
-var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
-if (basewrocketmeluncur < 1000) {
-var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
-leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
-scrolltoprocketmeluncur.style.left = ( basewrocketmeluncur + leftrocketmeluncur ) + 'px';
-} else {
-scrolltoprocketmeluncur.style.left = 'auto';
-scrolltoprocketmeluncur.style.right = '10px';
-}
-})
-
-jQuery('#rocketmeluncur').click(function(){
-    jQuery("html, body").animate({ scrollTop: '0px',display:'none'},{
-            duration: 600,  
-            easing: 'linear'
-        });
-    
-    var self = this;
-    this.className += ' '+"launchrocket";
-    setTimeout(function(){
-      self.className = 'showrocket';
-    },800)
+$(document).ready(function(){
+ //Check to see if the window is top if not then display button
+ $(window).scroll(function(){
+  // Show button after 100px
+  var showAfter = 100;
+  if ($(this).scrollTop() > showAfter ) { 
+   $('.back-to-top').fadeIn();
+  } else { 
+   $('.back-to-top').fadeOut();
+  }
+ });
+ 
+ //Click event to scroll to top
+ $('.back-to-top').hide().click(function(){
+  $('html, body').animate({scrollTop : 0},800);
+  return false;
+ });
+ 
 });
