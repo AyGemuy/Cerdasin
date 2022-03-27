47a0bb6b7aec38f62020a99e90ca354ef9f682a0
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-27 17:03:16 +0800

    x
    x

--- js/to-top.js
+++ js/to-top.js
@@ -1,8 +1,8 @@
 $(window).scroll(function() {
     if($(this).scrollTop() &gt; 200) {
-        $(&#39;#back-to-top&#39;).fadeIn();
+        $(&#39;.back-to-top&#39;).fadeIn();
     } else {
-        $(&#39;#back-to-top&#39;).fadeOut();
+        $(&#39;.back-to-top&#39;).fadeOut();
     }
 });
 
