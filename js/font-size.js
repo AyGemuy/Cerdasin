4c05aa7718b2d490c9cd976b69d39694a205b5b9
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 11:51:19 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,8 +1,3 @@
-function FungsiSize(PilihSize) {
-  var listSize = PilihSize.option[PilihSize.selectedIndex].text;
-  document.getElementById("fontArea").style.fontSize = listSize;
-}
-function FungsiWar(PilihWar) {
-  var listWar = PilihWar.option[PilihWar.selectedIndex].text;
-  document.getElementById("fontArea").style.color = listWar;
-}
+$("#fontSize").on("input",function () {
+            $('#name').css("font-size", $(this).val() + "px");
+    });
