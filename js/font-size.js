09e50902df85500cba461f9f88e78d99d97956fe
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 20:22:35 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,20 +1,3 @@
-/* Range */
-const ini_elements = document.querySelectorAll(['range-slider']);
-
-ini_elements.forEach(element => {
-  element.insertAdjacentHTML('afterend', `
-    <output>${element.value}</output>
-  `);
-});
-
-document.addEventListener('input', ini_e => {
-  const ini_input = ini_e.target;
-  const ini_output = ini_input.nextElementSibling;
-  if (output) {
-    ini_output.textContent = ini_input.value;
-  }
-});
-
 /* Font */
 $("#fontSize").change(function() {
 $('#fontArea').css("font-size", $(this).val() + "px");
