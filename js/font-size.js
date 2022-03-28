3e1b75b391680a4ffdd0a9cdf19b447982f298ec
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 13:55:16 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,58 +1,23 @@
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
 
-var volumeSlider = document.getElementById('volume');
-var scrubberSlider = document.getElementById('scrubber');
-var sliders = [volumeSlider, scrubberSlider];
-
-function Slider(slider) {
-  this.slider = slider;
-  slider.addEventListener('input', function() {
-    this.updateSliderOutput();
-    this.updateSliderLevel();
-  }.bind(this), false);
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
   
-  this.level = function() {
-    var level = this.slider.querySelector('.slider-input');
-    return level.value;
-  }
-  
-  this.levelString = function() {
-    return parseInt(this.level());
-  }
-  
-  this.remaining = function() {
-    return 99.5 - this.level();
-  }
-  
-  this.remainingString = function() {
-    return parseInt(this.remaining());
-  }
-  
-  this.updateSliderOutput = function() {
-    var output = this.slider.querySelector('.slider-output');
-    var remaining = this.slider.querySelector('.slider-remaining');
-    var thumb = this.slider.querySelector('.slider-thumb');
-    output.value = this.levelString();
-    output.style.left = this.levelString() + '%';
-    thumb.style.left = this.levelString() + '%';
-    if (remaining) { 
-      remaining.style.width = this.remainingString() + '%';
-    }
-  }
-  
-  this.updateSlider = function(num) {
-    var input = this.slider.querySelector('.slider-input');
-    input.value = num;
-  }
-  
-  this.updateSliderLevel =function() {
-    var level = this.slider.querySelector('.slider-level');
-    level.style.width = this.levelString() + '%';
-  }
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
 }
 
-sliders.forEach(function(slider) {
-  new Slider(slider);
-});
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
+
+numberInput.addEventListener('input', handleInputChange)
 
 $("#fontSize").on("input",function () {
             $('#fontArea').css("font-size", $(this).val() + "px");
