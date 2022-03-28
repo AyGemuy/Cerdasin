f7a9131b5842acf28dea934b52a45ca3decdc0d1
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 11:23:15 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,4 +1,14 @@
 function FungsiHuruf(PilihTag) {
-  var listValue = PilihTag.options[PilihTag.selectedIndex].text;
+  var listValue = PilihTag.input[PilihTag.selectedIndex].text;
   document.getElementById("fontArea").style.fontSize = listValue;
 }
+
+$('.dropdown-el').click(function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).toggleClass('expanded');
+  $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+$(document).click(function() {
+  $('.dropdown-el').removeClass('expanded');
+});
