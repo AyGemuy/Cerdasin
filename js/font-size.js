74df98ac84688cddec735417b8b08f9561595e0d
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 12:43:47 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,44 +1,11 @@
-var dataValue = document.querySelector('.value'),
-    input = document.querySelector('input.slider-input'),
-    $thumb = document.querySelector('.slider__thumb'),
-    $tail = document.querySelector('.slider__tail'),
-    $tooltip = document.querySelector('.tooltip'),
-    $innertooltip = document.querySelector('.innertooltip'),
-    test = document.querySelector('.test'),
-    lastValue;
-  
-(function init(){
-  input.setAttribute('value',0);
-  lastValue = input.getAttribute('value');
-  $tail.style.width = 0;
-  $innertooltip.innerHTML = 0;
-})();
+const input = document.querySelector("input");
+const label = document.querySelector("label");
 
-
-function update(event,value,maxValue){
-    lastValue = Number(value);
-    var move = (100/maxValue) * Number(value) +'%';
-    $tail.style.width = move;
-    $thumb.style.left = move;
-    $tooltip.style.left = move;
-    $innertooltip.innerHTML = Math.floor(lastValue);
-}
-
-function setMaxRate(){
-  
-  lastValue = 0;
-  
-  var max = document.querySelector('.setMax').value;
-  input.setAttribute('max', max);
-  document.querySelector('.setMax').value = '';
-
-  $tail.style.width = 0;
-  $thumb.style.left = 0;
-  $innertooltip.innerHTML = 0;
-  
-  $tooltip.style.left = 0;
-  input.setAttribute('value',0);
-}
+input.addEventListener("input", event => {
+  const value = Number(input.value) / 100;
+  input.style.setProperty("--thumb-rotate", `${value * 720}deg`);
+  label.innerHTML = Math.round(value * 50);
+});
 
 $("#fontSize").on("input",function () {
             $('#fontArea').css("font-size", $(this).val() + "px");
