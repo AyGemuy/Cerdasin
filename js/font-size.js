82f7f2d372848b2fde9597bb8883058b9606c1de
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 20:06:47 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -19,15 +19,7 @@ document.addEventListener('input', e => {
 $("#fontSize").change(function() {
             $('#fontArea').css("font-size", $(this).val() + "px");
     });
-$("#fontShadow").change(function() {
-            $('#fontArea').css("color", "px" + $(this).val());
-    });
-$("#fontWeight").change(function() {
-            $('#fontArea').css("font-weight", $(this).val());
-    });    
+
 $("#fontStyle").change(function() {
     $('#fontArea').css("font-family", $(this).val());
-});
-$("#borRad").change(function() {
-document.querySelectorAll(['border-radius']).val() + "px");
 });
