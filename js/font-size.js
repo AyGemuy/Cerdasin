ee6b218b0634b5cdd96700e6463d27a75f0f2125
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-28 21:42:36 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,5 +1,5 @@
 /* Range */
-const ini_elements = document.querySelectorAll(['range-slider']);
+const ini_elements = document.querySelector('.range-slider');
       ini_elements.forEach(ini_element => {
         ini_element.insertAdjacentHTML('afterend', `
           <ini_output>${ini_element.value}</ini_output>
