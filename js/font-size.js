e91b9e6e5c0acacd1e29a39d17541c1a2ac3e6a4
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-29 08:18:13 +0800

    x
    x

--- js/font-size.js
+++ js/font-size.js
@@ -1,20 +1,5 @@
-/* Slider */
-let rangeInput = document.querySelector(".range-input input");
-let rangeValue = document.querySelector(".range-input .value div");
 
-let  start = parseFloat(rangeInput.min);
-let end = parseFloat(rangeInput.max);
-let step = parseFloat(rangeInput.step);
-
-for(let i=start;i<=end;i+=step){
-  rangeValue.innerHTML += '<div>'+i+'</div>';
-}
-rangeInput.addEventListener("input",function(){
-  let top = parseFloat(rangeInput.value)/step * -40;
-  rangeValue.style.marginTop = top+"px";
-});
-
-/* Font */
+/* Font Setting */
 $("#fontSize").change(function() {
 $('#fontArea').css("font-size", $(this).val() + "px");
 });
