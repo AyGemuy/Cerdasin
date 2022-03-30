1fa637c6b6e43aaebf19d67eac6d0655ad97d286
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-31 06:13:57 +0800

    x
    x

--- js/total.js
+++ js/total.js
@@ -1,9 +1,7 @@
 ambil_data = readJsonFromUrl('//www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM');
-var post_nya = ambil_data.posts.totalItems;
-var page_nya = ambil_data.pages.totalItems;
 $("#total_post_nya").click(function() {
-  alert(post_nya);
+  alert(ambil_data.posts.totalItems);
 });
 $("#total_page_nya").click(function() {
-  alert(page_nya);
+  alert(ambil_data.pages.totalItems);
 });
