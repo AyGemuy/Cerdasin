2f24cea449b1cc2a343083fc9bf31b4f09999c71
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-31 06:33:55 +0800

    x
    x

--- js/total.js
+++ js/total.js
@@ -1,14 +1,8 @@
+$.getJSON('//www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM', function(data) {
 $('#total_post_nya').on('click', function () {
- fetch('https://www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM'')
- .then(response => response.json())
- .then(data => {
  alert(data.posts.totalItems)
- })
  });
- $('#total_page_nya').on('click', function () {
- fetch('https://www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM'')
- .then(response => response.json())
- .then(data => {
+ $('#total_post_nya').on('click', function () {
  alert(data.pages.totalItems)
- })
  });
+});
