c45038fcb935f3f36e9ea48a568db23c6f07ec56
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-31 06:39:52 +0800

    x
    x

--- js/total.js
+++ js/total.js
@@ -1,8 +1,8 @@
 $.getJSON('//www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM', function(data) {
 $('#total_post_nya').on('click', function () {
- alert('Total Postingan :' + data.posts.totalItems)
+ alert('<b>Total Postingan : </b> ' + data.posts.totalItems)
  });
  $('#total_page_nya').on('click', function () {
- alert('Total Postingan :' + data.pages.totalItems)
+ alert('<b>Total Halaman : </b> ' + data.pages.totalItems)
  });
 });
