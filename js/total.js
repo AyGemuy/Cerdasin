abbb4dca485b4e7a2870cf5bcd0f84cdc68531d6
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-03-31 06:19:31 +0800

    x
    x

--- js/total.js
+++ js/total.js
@@ -1,7 +1,14 @@
-ambil_data = readJsonFromUrl('//www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM');
-$("#total_post_nya").click(function() {
-  alert(ambil_data.posts.totalItems);
-});
-$("#total_page_nya").click(function() {
-  alert(ambil_data.pages.totalItems);
-});
+$('#total_post_nya').on('click', function () {
+ fetch('https://www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM'')
+ .then(response => response.json())
+ .then(data => {
+ alert(data.posts.totalItems)
+ })
+ });
+ $('#total_page_nya').on('click', function () {
+ fetch('https://www.googleapis.com/blogger/v3/blogs/8384880727344316369?key=AIzaSyBgkQwCIlXvdgPfaUPAlEF2Aok9NV-9tKM'')
+ .then(response => response.json())
+ .then(data => {
+ alert(data.pages.totalItems)
+ })
+ });
