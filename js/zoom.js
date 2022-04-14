05cb8b661ead042d1ec83ed72f2aeba748386cd6
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-14 21:06:11 +0800

    x
    x

--- js/zoom.js
+++ js/zoom.js
@@ -1,45 +1,113 @@
-jQuery(document).ready(function($) {
-    // Set initial zoom level
-    var zoom_level = 100;
+import Moveable from "moveable";
 
-    // Click events
-    $('#zoom_in').click(function() {
-        zoom_page(10, $(this))
-    });
-    $('#zoom_out').click(function() {
-        zoom_page(-10, $(this))
-    });
-    $('#zoom_reset').click(function() {
-        zoom_page(0, $(this))
-    });
+const moveable = new Moveable(document.body, {
+    target: document.querySelector(".target"),
+    // If the container is null, the position is fixed. (default: parentElement(document.body))
+    container: document.body,
+    draggable: true,
+    resizable: true,
+    scalable: true,
+    rotatable: true,
+    warpable: true,
+    // Enabling pinchable lets you use events that
+    // can be used in draggable, resizable, scalable, and rotateable.
+    pinchable: true, // ["resizable", "scalable", "rotatable"]
+    origin: true,
+    keepRatio: true,
+    // Resize, Scale Events at edges.
+    edge: false,
+    throttleDrag: 0,
+    throttleResize: 0,
+    throttleScale: 0,
+    throttleRotate: 0,
+});
+/* draggable */
+moveable.on("dragStart", ({ target, clientX, clientY }) => {
+    console.log("onDragStart", target);
+}).on("drag", ({
+    target, transform,
+    left, top, right, bottom,
+    beforeDelta, beforeDist, delta, dist,
+    clientX, clientY,
+}) => {
+    console.log("onDrag left, top", left, top);
+    target!.style.left = `${left}px`;
+    target!.style.top = `${top}px`;
+    // console.log("onDrag translate", dist);
+    // target!.style.transform = transform;
+}).on("dragEnd", ({ target, isDrag, clientX, clientY }) => {
+    console.log("onDragEnd", target, isDrag);
+});
 
-    // Zoom function
-    function zoom_page(step, trigger) {
-        // Zoom just to steps in or out
-        if (zoom_level >= 120 && step > 0 || zoom_level <= 80 && step < 0) return;
+/* resizable */
+moveable.on("resizeStart", ({ target, clientX, clientY }) => {
+    console.log("onResizeStart", target);
+}).on("resize", ({ target, width, height, dist, delta, clientX, clientY }) => {
+    console.log("onResize", target);
+    delta[0] && (target!.style.width = `${width}px`);
+    delta[1] && (target!.style.height = `${height}px`);
+}).on("resizeEnd", ({ target, isDrag, clientX, clientY }) => {
+    console.log("onResizeEnd", target, isDrag);
+});
 
-        // Set / reset zoom
-        if (step == 0) zoom_level = 100;
-        else zoom_level = zoom_level + step;
+/* scalable */
+moveable.on("scaleStart", ({ target, clientX, clientY }) => {
+    console.log("onScaleStart", target);
+}).on("scale", ({
+    target, scale, dist, delta, transform, clientX, clientY,
+}: OnScale) => {
+    console.log("onScale scale", scale);
+    target!.style.transform = transform;
+}).on("scaleEnd", ({ target, isDrag, clientX, clientY }) => {
+    console.log("onScaleEnd", target, isDrag);
+});
 
-        // Set page zoom via CSS
-        $('body').css({
-            transform: 'scale(' + (zoom_level / 100) + ')', // set zoom
-            transformOrigin: '50% 0' // set transform scale base
-        });
+/* rotatable */
+moveable.on("rotateStart", ({ target, clientX, clientY }) => {
+    console.log("onRotateStart", target);
+}).on("rotate", ({ target, beforeDelta, delta, dist, transform, clientX, clientY }) => {
+    console.log("onRotate", dist);
+    target!.style.transform = transform;
+}).on("rotateEnd", ({ target, isDrag, clientX, clientY }) => {
+    console.log("onRotateEnd", target, isDrag);
+});
 
-        // Adjust page to zoom width
-        if (zoom_level > 100) $('body').css({
-            width: (zoom_level * 1.2) + '%'
-        });
-        else $('body').css({
-            width: '100%'
-        });
+/* warpable */
+this.matrix = [
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+];
+moveable.on("warpStart", ({ target, clientX, clientY }) => {
+    console.log("onWarpStart", target);
+}).on("warp", ({
+    target,
+    clientX,
+    clientY,
+    delta,
+    dist,
+    multiply,
+    transform,
+}) => {
+    console.log("onWarp", target);
+    // target.style.transform = transform;
+    this.matrix = multiply(this.matrix, delta);
+    target.style.transform = `matrix3d(${this.matrix.join(",")})`;
+}).on("warpEnd", ({ target, isDrag, clientX, clientY }) => {
+    console.log("onWarpEnd", target, isDrag);
+});
 
-        // Activate / deaktivate trigger (use CSS to make them look different)
-        if (zoom_level >= 120 || zoom_level <= 80) trigger.addClass('disabled');
-        else trigger.parents('ul').find('.disabled').removeClass('disabled');
-        if (zoom_level != 100) $('#zoom_reset').removeClass('disabled');
-        else $('#zoom_reset').addClass('disabled');
-    }
+/* pinchable */
+// Enabling pinchable lets you use events that
+// can be used in draggable, resizable, scalable, and rotateable.
+moveable.on("pinchStart", ({ target, clientX, clientY }) => {
+    // pinchStart event occur before dragStart, rotateStart, scaleStart, resizeStart
+    console.log("onPinchStart");
+}).on("pinch", ({ target, clientX, clientY, datas }) => {
+    // pinch event occur before drag, rotate, scale, resize
+    console.log("onPinch");
+}).on("pinchEnd", ({ isDrag, target, clientX, clientY, datas }) => {
+    // pinchEnd event occur before dragEnd, rotateEnd, scaleEnd, resizeEnd
+    console.log("onPinchEnd");
 });
