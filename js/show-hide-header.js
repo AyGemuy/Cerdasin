1072d7d77f8f10062072b7177eca462416098cf7
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-19 15:52:36 +0800

    x
    x

--- js/show-hide-header.js
+++ js/show-hide-header.js
@@ -2,10 +2,10 @@ var mun_cul = window.pageYOffset;
 window.onscroll = function(){
 var hil_ang = window.pageYOffset;
 if (mun_cul > hil_ang){
-$("#header, #mega-button, #step-button, #HTML11, #mobile-menu").show();
+$("#header, #mega-button, #step-button, #HTML11, #mobile-menu").fadeIn();
 }
 else {
-$("#header, #mega-button, #step-button, #HTML11, #mobile-menu").hide();
+$("#header, #mega-button, #step-button, #HTML11, #mobile-menu").fadeOut();
 }
 mun_cul = hil_ang
 }
