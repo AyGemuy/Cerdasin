6e4264d1040f96d9d077af609756e3f281ece1aa
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-19 18:43:18 +0800

    x
    x

--- js/to-top.js
+++ js/to-top.js
@@ -1,19 +1,11 @@
-$(document).ready(function(){
- //Check to see if the window is top if not then display button
- $(window).scroll(function(){
-  // Show button after 100px
-  var showAfter = 100;
-  if ($(this).scrollTop() > showAfter ) { 
-   $('.back-to-top').fadeIn();
-  } else { 
-   $('.back-to-top').fadeOut();
-  }
- });
- 
- //Click event to scroll to top
- $('.back-to-top').hide().click(function(){
-  $('html, body').animate({scrollTop : 0},800);
-  return false;
- });
- 
+var scrollTimer = null;
+$(window).scroll(function() {
+$('.back-to-top')
+.fadeIn(100);
+if (scrollTimer !== null) {
+clearTimeout(scrollTimer);
+}
+scrollTimer = setTimeout(function() {
+$('.back-to-top').fadeOut();
+}, 1500);
 });
