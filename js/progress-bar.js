1d5f6ec3db123fb81406099662833e4ba73e59fc
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-19 18:28:40 +0800

    x
    x

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -4,8 +4,8 @@ $(window).scroll(function() {
 var viewportHeight = $(this).height(),
 scrollbarHeight = viewportHeight / $(document).height() * viewportHeight,
 progress = $(this).scrollTop() / ($(document).height() - viewportHeight),
-distance = progress * (viewportHeight - scrollbarHeight) + scrollbarHeight / 2 - $('#scrollPersentase').height() / 2;
-$('#scrollPersentase')
+distance = progress * (viewportHeight - scrollbarHeight) + scrollbarHeight / 2 - $('#scrollPersentase, #rocket-atas').height() / 2;
+$('#scrollPersentase, #rocket-atas')
 .css('top', distance)
 .text(' (' + Math.round(progress * 100) + '%)')
 .fadeIn(100);
@@ -13,7 +13,7 @@ if (scrollTimer !== null) {
 clearTimeout(scrollTimer);
 }
 scrollTimer = setTimeout(function() {
-$('#scrollPersentase').fadeOut();
+$('#scrollPersentase, #rocket-atas').fadeOut();
 }, 1500);
 });
 /*]]>*/
