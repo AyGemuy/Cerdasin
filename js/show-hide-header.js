da3f5343782e5d95091f39530a0dfa62671eef01
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-19 15:15:25 +0800

    x
    x

--- js/show-hide-header.js
+++ js/show-hide-header.js
@@ -1,10 +1,9 @@
-/*<![CDATA[*/
 var prevScrollpos = window.pageYOffset;window.onscroll = function(){
 var currentScrollPos = window.pageYOffset;if (prevScrollpos > currentScrollPos){
-doStuff(document.querySelectorAll("#header, #mega-button, #step-button").classList.remove('show'));
-} else{
-doStuff(document.querySelectorAll("#header, #mega-button, #step-button").classList.add('show'));
+document.querySelectorAll("#header, #mega-button, #step-button").classList.remove('show');
+}
+else {
+document.querySelectorAll("#header, #mega-button, #step-button").classList.add('show');
 }
 prevScrollpos = currentScrollPos
 }
-/*]]>*/
