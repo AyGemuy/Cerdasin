c7447652b6d5711a5ef54f34c93096000a3d8e4a
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-21 05:23:28 +0800

    x
    x

--- js/grup-sc.js
+++ js/grup-sc.js
@@ -1,15 +1,18 @@
+/* Loader */
 $(document).ready(function() {
 	setTimeout(function(){
 		$('body').addClass('loaded');
 	}, 3000);
 });
 
+/* Twilt */
 $("#header, #mega-button, #step-button, #mobile-menu, #HTML2, #step1, #step2, #step4, #step5, #step6, #step7, #step8, #step10, #fontstyle, #qrgen, #ex8, #ex9, #nav-widget-2, #ckBox, #Label00, #tocTilt, #PopularPosts00, #HTML1, #HTML5, #HTML3,  #FeaturedPost1, #nav-widget-1, #ntfTilt").tilt({
     glare: true,
     scale: 1,
     maxGlare: .3
 })
 
+/* Style */
 deco = document.querySelectorAll('.decorated');
 deco.forEach(function(element) {
 	element.setAttribute('data-text', element.textContent);
