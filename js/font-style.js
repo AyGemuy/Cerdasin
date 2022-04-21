091ff2834716866dadea769afce77216b640ef7f
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-22 05:12:34 +0800

    x
    x

--- js/font-style.js
+++ js/font-style.js
@@ -56,26 +56,3 @@ $element
 function updateHandle(el, val) {
   el.textContent = val;
 }
-
-$(function() {
-    var $document   = $(document),
-        $inputRange = $('input[type="range"]');
-    
-    // Example functionality to demonstrate a value feedback
-    function valueOutput(element) {
-        var value = element.value,
-            output = element.parentNode.getElementsByTagName('output')[0];
-        output.innerHTML = value;
-    }
-    for (var i = $inputRange.length - 1; i >= 0; i--) {
-        valueOutput($inputRange[i]);
-    };
-    $document.on('input', 'input[type="range"]', function(e) {
-        valueOutput(e.target);
-    });
-    // end
-  
-    $inputRange.rangeslider({
-      polyfill: false 
-    });
-});
