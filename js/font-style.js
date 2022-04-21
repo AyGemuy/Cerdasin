c2c4fe4c451f7a97707bb17abc4e041a40166bd7
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-22 05:07:55 +0800

    x
    x

--- js/font-style.js
+++ js/font-style.js
@@ -38,6 +38,25 @@ $('#fontArea').toggleClass( "decorated sample-8" );
 });
 
 /* Slider Range */
+var $element = $('input[type="range"]');
+
+$element
+  .rangeslider({
+    polyfill: false,
+    onInit: function() {
+      var $handle = $('.rangeslider__handle', this.$range);
+      updateHandle($handle[0], this.value);
+    }
+  })
+  .on('input', function(e) {
+    var $handle = $('.rangeslider__handle', e.target.nextSibling);
+    updateHandle($handle[0], this.value);
+  });
+
+function updateHandle(el, val) {
+  el.textContent = val;
+}
+
 $(function() {
     var $document   = $(document),
         $inputRange = $('input[type="range"]');
