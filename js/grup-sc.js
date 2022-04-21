f9e6ef5f7bc2b368b1dba9d8e64007b56d9fbb7b
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-21 12:27:40 +0800

    x
    x

--- js/grup-sc.js
+++ js/grup-sc.js
@@ -1,8 +1,9 @@
 /* Loader */
-$(document).ready(function() {
-	setTimeout(function(){
-		$('body').addClass('loaded');
-	}, 3000);
+$(document).ready(function(){
+  setTimeout(function(){
+    $('body').addClass('loaded');
+    $('h1').css('color','#222222')
+  }, 1000);
 });
 
 /* Twilt */
