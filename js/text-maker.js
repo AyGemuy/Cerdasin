801f3dcadc6dc6c7fe8447c74941c0b600547cbe
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-25 20:12:04 +0800

    x
    x

--- js/text-maker.js
+++ js/text-maker.js
@@ -1,10 +1,20 @@
-$("#btn_asu").click(function(){
-  $.getJSON("https://api-xfar05.herokuapp.com/api/asupan")
-  .done(function(data){
-    $("#vid_asu").attr('src', data.result.result);
-  })
-  .fail(function(){
-    alert(" An error  just happened")
-  })
+$.getJSON('//api-xfar05.herokuapp.com/api/asupan', function(data) {
+$('#btn_asu').on('click', function () {
+Swal.fire({
+  title: '<strong>Asupan</strong>',
+  icon: 'success',
+  html:
+    '<iframe width="400" height="400" src="{{data.result.result}}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+  showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Great!',
+  confirmButtonAriaLabel: 'Thumbs up, great!',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i>',
+  cancelButtonAriaLabel: 'Thumbs down'
 })
+ });
+});
 
