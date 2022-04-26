64c3bbfb5da2903c79c7a0a77d5516c1f1312a05
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-26 16:01:41 +0800

    x
    x

--- js/text-maker.js
+++ js/text-maker.js
@@ -2,49 +2,49 @@ $('#btn_asu').on('click', function () {
 $.getJSON('//api-xfar05.herokuapp.com/api/asupan', function(asupanr) {
 swals.fire({
   title: 'Hasil',
-  html: 'Link :<a href="{{asupanr.result.result}}"></a>',
+  html: 'Link :<a href="+ asupanr.result.result +"></a>',
 })
 })
 });
 
 $('#btn_waifu').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Waifumu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/waifu',
 })
 });
 
 $('#btn_neko').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Nekomu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/neko',
 })
 });
 
 $('#btn_husbu').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Husbumu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/husbu',
 })
 });
 
 $('#btn_loli').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Lolimu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/loli',
 })
 });
 
 $('#btn_milf').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Milfmu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/milf',
 })
 });
 
 $('#btn_cosplay').on('click', function () {
 swals.fire({
-  title: 'Hasil',
+  title: 'Cosplaymu!',
   imageUrl: '//api-xfar05.herokuapp.com/api/randomimage/cosplay',
 })
 });
