d3d66a42ec3082c078119c017c70f0733a18bd4c
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-26 15:31:27 +0800

    x
    x

--- js/text-maker.js
+++ js/text-maker.js
@@ -1,19 +1,6 @@
 $.getJSON('//api-xfar05.herokuapp.com/api/asupan', function(asupanr) {
-$('#btn_asu').on('click', function () {
 Swal.fire({
-  title: '<strong>Asupan</strong>',
+  title: 'Hasil : <a>'+ asupanr.result.result +'</a>',
   icon: 'success',
-  html:
-    '<iframe width="400" height="400" src="{{asupanr.result.result}}"></iframe>',
-  showCloseButton: true,
-  showCancelButton: true,
-  focusConfirm: false,
-  confirmButtonText:
-    '<i class="fa fa-thumbs-up"></i> Great!',
-  confirmButtonAriaLabel: 'Thumbs up, great!',
-  cancelButtonText:
-    '<i class="fa fa-thumbs-down"></i>',
-  cancelButtonAriaLabel: 'Thumbs down'
 })
- });
 });
