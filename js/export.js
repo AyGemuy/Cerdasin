648f93c6f7f4f395982ddc7154670810fb13d1c9
Wudysoft <91945119+AyGemuy@users.noreply.github.com>
2022-04-27 09:39:22 +0800

    x
    x

--- js/export.js
+++ js/export.js
@@ -1,13 +1,11 @@
-new JotformFeedback({
-        formId: "221158032315040",
-        buttonText: "Post",
-        base: "https://form.jotform.com/",
-        background: "#04a0fb",
-        fontColor: "#FFFFFF",
-        buttonSide: "left",
-        buttonAlign: "center",
-        type: false,
-        width: 700,
-        height: 500,
-        isCardForm: false
-      });
+var JFL_221158032315040 = new JotformFeedback({
+          formId: '221158032315040',
+          base: 'https://form.jotform.com/',
+          windowTitle: 'Post Artikel',
+          background: '#0c99ea',
+          fontColor: '#FFFFFF',
+          type: '0',
+          height: 500,
+          width: 700,
+          openOnLoad: false
+        });
