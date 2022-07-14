912bf718c27bf9f4d378fa2fe4b964568d76e65c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-07-14 19:59:37 +0800

    New
    New

---
+++ php/bot.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center>
+<h1>Experiment to run bash script after pressing html button in browser</h1>
+<button type="button" onclick="window.location.href = 'http://<?php echo $_SERVER['HTTP_HOST']; ?>/php/index.php';">Click Me!</button>
+</center>
+
+</body>
+</html>
